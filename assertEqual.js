11b1c345575ee5497602ba830b7b7b2d6a7daab2
Gaurav Mahant <thegau22@gmail.com>
2022-09-20 18:58:24 -0600

    My own console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log("🟢 Assertion Passed: " + actual + " === " + expected);
+  }
+
+  if (actual !== expected) {
+    console.log("🔴 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
