f7325d818295b9689b09f0ba159b3a5aa77b5666
Gaurav Mahant <thegau22@gmail.com>
2022-09-25 15:02:28 -0600

    function to return the middle value(s) of an array

---
+++ middle.js
@@ -0,0 +1,52 @@
+const eqArrays = function (array1, array2) {
+  if (array1 == null || array2 == null) { return false};
+  if (array1.length !== array2.length) { return false}; 
+  for (let i = 0; i < array1.length; i++) {
+    const element = array1[i];
+      if (element !== array2[i]) {
+          return false;
+      }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (eqArrays) {
+  if (eqArrays) {
+    console.log(`🟢 Assertion Passed: Arrays are equal === ${eqArrays}`);
+  }
+  if (!eqArrays) {
+    console.log(`🔴 Assertion Failed: Arrays are equal !== ${eqArrays}`);
+  }
+}
+
+const eqArrays = function (array1, array2) {
+  if (array1 == null || array2 == null) { return false};
+  if (array1.length !== array2.length) { return false}; 
+  for (let i = 0; i < array1.length; i++) {
+    const element = array1[i];
+      if (element !== array2[i]) {
+          return false;
+      }
+  }
+  return true;
+}
+
+const middle = function (array) {
+  let newArray = [];
+  
+  if (array.length < 3) {
+    return newArray;
+  }
+
+  if (array.length % 2 !== 0) {
+    let i = Math.floor((array.length / 2);
+    newArray.push(array[i]);
+  }
+
+  if (array.length % 2 === 0) {
+    let x = ((array.length / 2) - 1);
+    let y = (array.length / 2);
+    newArray.push(array[x], array[y]);
+  }
+  return newArray;
+}
