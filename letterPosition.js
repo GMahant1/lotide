83329b6db8a05d64adcbdb10e74cbf0db45afab5
Gaurav Mahant <thegau22@gmail.com>
2022-09-29 21:09:38 -0600

    function to return letter index on each occurence in provided string, returns object

---
+++ letterPosition.js
@@ -0,0 +1,27 @@
+const test = "hello world";
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+}
+
+  return results;
+};
+
+
+console.log(letterPositions(test));
+
+
+
+// results[sentence[0]] = result[sentence[0]].push(i);
+// result = {h: [0]}
+
+// result[h].push()
