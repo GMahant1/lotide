460b94858b9fece8aa927d9778ab67be3b162972
Gaurav Mahant <thegau22@gmail.com>
2022-09-30 15:59:07 -0600

    function to check if two objects are identical, supports arrays as values

--- eqArrays.js
+++ eqArrays.js
@@ -21,9 +21,13 @@ const eqArrays = function (array1, array2) {
   return true;
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
+
+
+
+console.log(eqArrays(["2", 3, 4], ["2", 3]));
