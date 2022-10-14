5e226973c58444a663c4822a45f1893a60d4d469
Gaurav Mahant <thegau22@gmail.com>
2022-10-13 18:34:19 -0600

    refactored eqArrays to test properly

--- eqArrays.js
+++ eqArrays.js
@@ -21,13 +21,16 @@ const eqArrays = function (array1, array2) {
   return true;
 }
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
 
 
+// assertEqual(eqArrays(["2", 3, 4], ["2", 3]), false);
 
-console.log(eqArrays(["2", 3, 4], ["2", 3]));
+module.exports = eqArrays;
