4e92596394d5d88a7d0335ac44c6cc9e3980373c
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 15:59:08 -0600

    add check for undefined input

--- head.js
+++ head.js
@@ -1,7 +1,15 @@
 // function that takes an array as input and returns the first element 
 
 const head = function(array) {
+
+  if (!Array.isArray(array)) {
+    return "Input is not a valid array";
+  }
+
+  if (Array.isArray(array))
+
   return array[0];
 }
 
 module.exports = head;
+
