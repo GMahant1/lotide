db1bf22f4c252d5bbb45549ba15fd9ed33fedadf
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 16:19:27 -0600

    import eqArrays function, remove extra code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,30 +1,16 @@
+//import eqArrays function 
 const eqArrays = require("./eqArrays");
 
-// const eqArrays = function (array1, array2) {
-//   if (array1 == null || array2 == null) { return false};
-//   if (array1.length !== array2.length) { return false}; 
-//   for (let i = 0; i < array1.length; i++) {
-//     const element = array1[i];
-//       if (element !== array2[i]) {
-//           return false;
-//       }
-//   }
-//   return true;
-// }
+//assertion test that checks if two arrays are equal 
+const assertArraysEqual = function(array1, array2) {
 
-const assertArraysEqual = function (eqArrays) {
-  if (eqArrays) {
-    console.log(`🟢 Assertion Passed: Arrays are equal === ${eqArrays}`);
+  if (eqArrays(array1, array2)) {
+    console.log(`🟢 Assertion Passed: ${array1} === ${array2}`);
   }
-  if (!eqArrays) {
-    console.log(`🔴 Assertion Failed: Arrays are not equal !== ${eqArrays}`);
+
+  if (!eqArrays(array1, array2)) {
+    console.log(`🔴 Assertion Failed: ${array1} !== ${array2}`);
   }
-}
+};
 
 module.exports = assertArraysEqual;
-
-// assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-// assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
