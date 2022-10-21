72ba3eee03325f205bee20324f7862102a1f91da
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 15:53:59 -0600

    add check for undefined input

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,10 @@
 // function used to check if two values are equal, returns true if equal and false otherwise
 
 const assertEqual = function(actual, expected) {
+
+  if (typeof actual === "undefined" || typeof expected === "undefined" ) {
+    console.log("Input is undefined");
+  }
   
   if (actual === expected) {
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
