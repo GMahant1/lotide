9c399d98cb710596ad1e85fd42a421358577bb83
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 17:13:12 -0600

    remove duplicate/unnecessary code, add function description

--- countOnly.js
+++ countOnly.js
@@ -1,20 +1,9 @@
-const assertEqual = function(actual, expected) {
-  
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  }
+//function takes in a collection of items and returns a specific subset of those items
+const countOnly = function(allItems, itemsToCount) {
 
-  if (actual !== expected) {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-const countOnly = function (allItems, itemsToCount) {
   const result = {};
 
-  for(const item of allItems) {
-    console.log(item);
+  for (const item of allItems) {
 
     if (itemsToCount[item]) {
       if (result[item]) {
@@ -25,27 +14,7 @@ const countOnly = function (allItems, itemsToCount) {
     }
   }
 
-  return result; 
+  return result;
 };
-
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-//const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-// console.log(assertEqual(result1["Jason"], 1));
-// console.log(assertEqual(result1["Karima"], undefined));
-// console.log(assertEqual(result1["Fang"], 2));
-// console.log(assertEqual(result1["Agouhanna"], undefined)); 
 
 module.exports = countOnly;
