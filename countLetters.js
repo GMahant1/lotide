4b23268f28495751076b76b34ac43de6da6a43ee
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 17:26:04 -0600

    remove duplicate/unnecessary code, add notes for assumption made

--- countLetters.js
+++ countLetters.js
@@ -1,31 +1,19 @@
-const assertEqual = function(actual, expected) {
-  
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  }
+//function that takes in string and returns a count of each of the letters in string 
+const countLetters = function(string) {
 
-  if (actual !== expected) {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const testString = "This is a test string"
-
-const countLetters = function (string) {
   const newObject = {};
-
+  //assume case for letters does not matter , treat "T" === "t"
   for (const letters of string) {
-    //console.log(letters);
+
     if (newObject[letters]) {
-      newObject[letters] += 1
+      newObject[letters] += 1;
     } else {
       newObject[letters] = 1;
     }
     // newObject[letters] is to access each value from the for loop, if it exist increase the count by 1 otherwise assign a base value of 1
   }
-  return newObject;
-}
 
-//console.log(countLetters(testString));
+  return newObject;
+};
 
 module.exports = countLetters;
