9954af4c990d8459921557adf9dc4f0f763d4aa8
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 16:12:34 -0600

    add check for undefined input, remove duplicate code

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,14 @@
-const assertEqual = function(actual, expected) {
-  
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  }
-
-  if (actual !== expected) {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+//function takes in two arrays and returns true if they are equal or false otherwise
 const eqArrays = function (array1, array2) {
+
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {return false};
+
   if (array1 == null || array2 == null) { return false};
+
   if (array1.length !== array2.length) { return false}; 
+
   for (let i = 0; i < array1.length; i++) {
+    
     const element = array1[i];
       if (element !== array2[i]) {
           return false;
@@ -20,18 +16,5 @@ const eqArrays = function (array1, array2) {
   }
   return true;
 }
-
-module.exports = eqArrays;
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-
-
-// assertEqual(eqArrays(["2", 3, 4], ["2", 3]), false);
 
 module.exports = eqArrays;
