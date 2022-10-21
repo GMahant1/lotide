3a1463dbad83affaeb0221a3a65038dc147507c5
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 16:55:46 -0600

    fix code to work with all cases, add check for undefined input

--- without.js
+++ without.js
@@ -1,40 +1,19 @@
-const eqArrays = function (array1, array2) {
-  if (array1 == null || array2 == null) { return false};
-  if (array1.length !== array2.length) { return false}; 
-  for (let i = 0; i < array1.length; i++) {
-    const element = array1[i];
-      if (element !== array2[i]) {
-          return false;
-      }
-  }
-  return true;
-}
+//function that takes in a sourceArray and an itemsToRemoveArray, returns only items in sourceArray that are not in the itemsToRemoveArray
+const without = function(sourceArray, itemsToRemoveArray) {
 
-const assertArraysEqual = function (eqArrays) {
-  if (eqArrays) {
-    console.log(`🟢 Assertion Passed: Arrays are equal === ${eqArrays}`);
-  }
-  if (!eqArrays) {
-    console.log(`🔴 Assertion Failed: Arrays are equal !== ${eqArrays}`);
-  }
-}
-
-const without = function (array1, array2) {
   let newArray = [];
-  for (let i = 0; i < array1.length; i++ ) {
-    if (array1[i] !== array2[i]) {
-      newArray.push(array1[i]);
-    }
+
+  if (!Array.isArray(sourceArray) || !Array.isArray(itemsToRemoveArray)) {
+    return "Input is not a valid array";
   }
+
+  sourceArray.forEach(element => {
+    if (!itemsToRemoveArray.includes(element)) {
+      newArray.push(element);
+    }
+  });
+
   return newArray;
-}
-
-//console.log(without([1, 2, 3], [1]));
-//console.log(without(["1", "2", "3"], [1, 2, "3"]));
-
-//const words = ["hello", "world", "lighthouse"];
-//without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-//assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+};
 
 module.exports = without;
