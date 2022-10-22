7d43ed911a7666fcfebf74a60c10265410304545
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 18:48:32 -0600

    add check for undefined input, add note explaining callback behavior

--- takeUntil.js
+++ takeUntil.js
@@ -1,24 +1,22 @@
+//function that takes in an array and a callback and returns the first element that meets the criteria specified in callback
 const takeUntil = function (array, callback) {
+
   let newArray = [];
+
+  if (!Array.isArray(array)) {
+    return "Input is not a valid array";
+  }
   
   for (let item of array) {
+    //loop through array until the call back equates to false, push everthing looped so far
     if(!callback(item)) {
       newArray.push(item);
     } else {
       break;
     }
   }
+
   return newArray; 
 }
-
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);
-
-// console.log('---');
-
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// const results2 = takeUntil(data2, x => x === ',');
-// console.log(results2);
 
 module.exports = takeUntil;
