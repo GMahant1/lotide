16cb2cbb43f00903a24e532b8d10952a4f39eeb5
Gaurav Mahant <thegau22@gmail.com>
2022-10-21 18:43:54 -0600

    add check for undefined input, removed test code

--- map.js
+++ map.js
@@ -1,14 +1,18 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
+//function that takes in an array and a callback and returns a new array based on callback results
 const map = function (array, callback) {
+
   const results = [];
+
+  if (!Array.isArray(array)) {
+    return "Input is not a valid array";
+  }
+
   for (let item of array) {
     results.push(callback(item));
   }
+  
   return results;
 };
 
-//const results1 = map(words, word => word[0]);
-//console.log(results1);
 
 module.exports = map;
